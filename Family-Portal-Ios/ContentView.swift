86ac4c4019ca6760314377b0581d63cc48072069
Steven Grissom <sggrissom@gmail.com>
2026-01-22 21:47:40 -0600

    Initial Commit

---
+++ Family-Portal-Ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Family-Portal-Ios
+//
+//  Created by Grissom on 1/22/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Family-Portal-Ios/Family_Portal_IosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Family_Portal_IosApp.swift
+//  Family-Portal-Ios
+//
+//  Created by Grissom on 1/22/26.
+//
+
+import SwiftUI
+
+@main
+struct Family_Portal_IosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
