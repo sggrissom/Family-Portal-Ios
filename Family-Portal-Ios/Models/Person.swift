4196f75324868c6f2a090e35a472aaefde815577
Steven Grissom <sggrissom@gmail.com>
2026-01-25 12:14:36 -0600

    resolving remaining issues, successful deploy

--- Family-Portal-Ios/Models/Person.swift
+++ Family-Portal-Ios/Models/Person.swift
@@ -3,35 +3,48 @@ import SwiftData
 
 @Model
 final class Person {
-    var id: UUID
-    var remoteId: String?
+    var id: UUID = UUID()
+    var remoteId: String? = nil
     var name: String
     var type: PersonType
     var gender: Gender
-    var birthday: Date?
-    var profilePhotoId: Int?
+    var birthday: Date? = nil
+    var profilePhotoId: Int? = nil
 
-    var family: Family?
+    var family: Family? = nil
 
-    @Relationship(deleteRule: .cascade, inverse: \GrowthData.person)
-    var growthData: [GrowthData]
+    @Relationship(deleteRule: .cascade)
+    var growthData: [GrowthData] = []
 
-    @Relationship(deleteRule: .cascade, inverse: \Milestone.person)
-    var milestones: [Milestone]
+    @Relationship(deleteRule: .cascade)
+    var milestones: [Milestone] = []
 
-    var photos: [Photo]
-
-    init(name: String, type: PersonType, gender: Gender, birthday: Date? = nil) {
-        self.id = UUID()
-        self.remoteId = nil
+    @Relationship(deleteRule: .nullify)
+    var photos: [Photo] = []
+    
+    init(
+        id: UUID = UUID(),
+        remoteId: String? = nil,
+        name: String,
+        type: PersonType,
+        gender: Gender,
+        birthday: Date? = nil,
+        profilePhotoId: Int? = nil,
+        family: Family? = nil,
+        growthData: [GrowthData] = [],
+        milestones: [Milestone] = [],
+        photos: [Photo] = []
+    ) {
+        self.id = id
+        self.remoteId = remoteId
         self.name = name
         self.type = type
         self.gender = gender
         self.birthday = birthday
-        self.profilePhotoId = nil
-        self.family = nil
-        self.growthData = []
-        self.milestones = []
-        self.photos = []
+        self.profilePhotoId = profilePhotoId
+        self.family = family
+        self.growthData = growthData
+        self.milestones = milestones
+        self.photos = photos
     }
 }
